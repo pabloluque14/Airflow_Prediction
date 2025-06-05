410bc67062d71f3c7528edd4d355c6f2dffb2f08
Pablo Luque Moreno <pablolmoreno19@gmail.com>
2025-06-05 14:59:34 +0200

    Fix data merge to label columns

--- airflow/functions.py
+++ airflow/functions.py
@@ -20,19 +20,13 @@ def mergeData(**kwargs):
     df_humidity = pd.read_csv(kwargs['hum_file'], sep=',')
     df_temperature = pd.read_csv(kwargs['temp_file'], sep=',')
 
-
-    humidity = df_humidity['San Francisco']
-    date = list(df_humidity['datetime'])
-    temperature = df_temperature['San Francisco']
-
-    df = pd.DataFrame(date, columns = ['date'])
-
-
-    df = df.join(humidity)
-
-    df = df.join(temperature, lsuffix = ' temperature')
-
-    df.columns=['DATE','TEMP', 'HUM']
+    # Build dataframe using explicit column names so that
+    # TEMP comes from the temperature file and HUM from the humidity file
+    df = pd.DataFrame({
+        'DATE': df_humidity['datetime'],
+        'TEMP': df_temperature['San Francisco'],
+        'HUM': df_humidity['San Francisco']
+    })
 
     df.to_csv("/tmp/workflow/data.csv", encoding='utf8', index=False, sep=";")
 
